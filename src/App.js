6a714e9944394029402e98f554bd17256d3c5067
Sandeep Balaji <sandeep.balaji@rostr.in>
2019-09-14 17:28:59 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -79,15 +79,6 @@ function App() {
         <MapContainer />
       </main>
       {/* Footer */}
-      <footer className={classes.footer}>
-        <Typography variant="h6" align="center" gutterBottom>
-          Footer
-        </Typography>
-        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-          Something here to give the footer a purpose!
-        </Typography>
-        <Copyright />
-      </footer>
       {/* End footer */}
     </React.Fragment>
   );
